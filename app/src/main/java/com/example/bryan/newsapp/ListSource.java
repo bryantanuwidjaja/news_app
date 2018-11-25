de8c27ec3674e2ac3647733e1e64de116763f6e3
Bryan <bryantanuwijaya12@gmail.com>
2018-11-25 23:59:07 +0700

    Added HomeScreen

---
+++ app/src/main/java/com/example/bryan/newsapp/ListSource.java
@@ -0,0 +1,13 @@
+package com.example.bryan.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListSource extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_source);
+    }
+}
--- app/src/main/java/com/example/bryan/newsapp/MainActivity.java
+++ app/src/main/java/com/example/bryan/newsapp/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.bryan.newsapp;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.transition);
+        ImageView imageView_Logo = findViewById(R.id.imageView_MainActivity_Logo);
+        TextView textView_Title = findViewById(R.id.textView_MainActivity_Title);
+        imageView_Logo.startAnimation(animation);
+        textView_Title.startAnimation(animation);
+        final Intent i = new Intent(this,ListSource.class);
+        Thread timer = new Thread()
+        {
+            public void run ()
+            {
+                try
+                {
+                    sleep(5000);
+                } catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
